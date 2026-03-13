d2ba13e5399c058d81e9490a1c73bfb100f4bb8c
J. Scott Miller <j.scott.miller@gmail.com>
2026-03-13 14:33:39 -0500

    Fix missing clixVersion in config/nixos flake

--- config/nixos/flake.nix
+++ config/nixos/flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.clix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit self; };
+      specialArgs = { inherit self; clixVersion = self.shortRev or self.dirtyShortRev or "dev"; };
       modules = [
         ./base-configuration.nix
         ./configuration.nix
